73ad2fa7f223ae08fbdea8d4e1768d95f1a93a43
Arnold Foster <arniejf@googlemail.com>
2021-12-16 22:54:09 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
         </div>
 
         {/* todo use songkick api, there is a key in inbox */}
-        <GigList gigs={[{location: "The Old Courts, Wigan", date: "03/12/2021", link: "https://www.theoldcourts.com/shows/flash-a-tribute-to-queen/", linkText: "Event Info/Tickets"}]} />
+        <GigList gigs={[{location: "The Boulevard, Wigan", date: "17/12/2021", link: "https://www.boulevard-wigan.co.uk/events/afternoon-people-wild-signs-shackites-1", linkText: "Event Info/Tickets"}]} />
 
         <div className={styles.latestUpdate}>
             <div className={styles.latestUpdateText}>Until then, here's our latest video!</div>
