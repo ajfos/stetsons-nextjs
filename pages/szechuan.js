1cc739d63596ed26244137781be7cfbb9d19e2f5
Arnold Foster <arniejf@googlemail.com>
2024-06-29 10:45:04 +0100

    szechuan redirect

---
+++ pages/szechuan.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import styles from "../styles/redirectpage.module.scss";
+
+export default function Szechuan() {
+  useEffect(() => {
+    location.replace("https://hypeddit.com/szechuan-chicken");
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      <div>Heading to the home of Szechuan...</div>
+      <div>
+        If nothing happens, try clicking{" "}
+        <a href="https://hypeddit.com/szechuan-chicken" className={styles.link}>
+          here.
+        </a>
+      </div>
+    </div>
+  );
+}
